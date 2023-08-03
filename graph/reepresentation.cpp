c5819b1c272aae5a1e1464262a0ebe6c7c7d741c
Yash-devasp <yashgoswami148@gmail.com>
2023-08-03 21:19:35 +0530

    graph representation

--- graph/reepresentation.cpp
+++ graph/reepresentation.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n,m;
     cin >> n >> m;
-    // graph here
+    // adjacency matrix graph here
     int adj[n+1][m+1];
     for(int i=0;i < m;i++){
         int u, v;
@@ -12,4 +12,14 @@ int main() {
         adj[u][v] = 1;
         adj[v][u] = 1;
     }
+
+    // adjacency list
+    vector<int> adjL[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin >> u >> v;
+        adjL[u].push_back(v);
+        adjL[v].push_back(u);
+    }
+    return 0;
 }
